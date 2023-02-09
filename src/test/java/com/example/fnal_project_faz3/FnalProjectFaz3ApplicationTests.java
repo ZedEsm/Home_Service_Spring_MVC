ba7db799c675f5c0094d46a0f82098dd85082806
sazed <zedesm23@gmail.com>
2023-02-09 17:14:00 +0330

    initial commit

---
+++ src/test/java/com/example/fnal_project_faz3/FnalProjectFaz3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fnal_project_faz3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FnalProjectFaz3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
