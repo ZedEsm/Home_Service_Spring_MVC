108340313ea88949ec22b99c971223a8a3eee373
sazed <zedesm23@gmail.com>
2023-03-18 00:14:15 +0330

    done work by expert

--- src/main/java/com/example/final_project_faz3/maktab/ir/controller/ExpertController.java
+++ src/main/java/com/example/final_project_faz3/maktab/ir/controller/ExpertController.java
@@ -7,9 +7,12 @@ import com.example.final_project_faz3.maktab.ir.data.repository.ExpertRepository
 import com.example.final_project_faz3.maktab.ir.exceptions.ExpertExistenceException;
 import com.example.final_project_faz3.maktab.ir.exceptions.OrderExistenceException;
 import com.example.final_project_faz3.maktab.ir.service.ExpertService;
+import com.example.final_project_faz3.maktab.ir.service.OfferService;
 import com.example.final_project_faz3.maktab.ir.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/expert")
@@ -18,12 +21,14 @@ public class ExpertController {
     private final ExpertRepository expertRepository;
     private final OrderService orderService;
 
+    private final OfferService offerService;
     @Autowired
     public ExpertController(ExpertService expertService,
-                            ExpertRepository expertRepository, OrderService orderService) {
+                            ExpertRepository expertRepository, OrderService orderService, OfferService offerService) {
         this.expertService = expertService;
         this.expertRepository = expertRepository;
         this.orderService = orderService;
+        this.offerService = offerService;
     }
 
     @PostMapping("/postExpert")
@@ -40,5 +45,18 @@ public class ExpertController {
         Expert expert = expertRepository.findExpertById(expertId).orElseThrow(() -> new ExpertExistenceException("expert not found"));
         Orders orders = orderService.findOrderById(orderId).orElseThrow(() -> new OrderExistenceException("order not found"));
         expertService.addOffer(offers, expert, orders);
+    }
+
+    @GetMapping("/expertDone/{orderId}")
+    public void expertDone(@PathVariable Long orderId,@RequestParam(required = false)Long offerId ){
+        try {
+            Optional<Orders> orders = Optional.ofNullable(orderService.findOrderById(orderId).orElseThrow(() -> new OrderExistenceException("order does not exist")));
+            Optional<Offers> offers = offerService.findOfferById(offerId);
+            expertService.expertDone(orders,offers);
+        } catch (OrderExistenceException e) {
+            System.out.println(e.getMessage());
+        }
+
+
     }
 }
--- src/main/java/com/example/final_project_faz3/maktab/ir/service/ExpertService.java
+++ src/main/java/com/example/final_project_faz3/maktab/ir/service/ExpertService.java
@@ -1,19 +1,19 @@
 package com.example.final_project_faz3.maktab.ir.service;
 
-import com.example.final_project_faz3.maktab.ir.data.model.entity.Expert;
-import com.example.final_project_faz3.maktab.ir.data.model.entity.Offers;
-import com.example.final_project_faz3.maktab.ir.data.model.entity.Orders;
-import com.example.final_project_faz3.maktab.ir.data.model.entity.SubService;
+import com.example.final_project_faz3.maktab.ir.data.model.entity.*;
 import com.example.final_project_faz3.maktab.ir.data.model.enumeration.ExpertScore;
 import com.example.final_project_faz3.maktab.ir.data.model.enumeration.ExpertStatus;
 import com.example.final_project_faz3.maktab.ir.data.model.enumeration.OrderStatus;
 import com.example.final_project_faz3.maktab.ir.data.repository.ExpertRepository;
 import com.example.final_project_faz3.maktab.ir.exceptions.ExpertExistenceException;
+import com.example.final_project_faz3.maktab.ir.exceptions.OrderExistenceException;
 import com.example.final_project_faz3.maktab.ir.util.validation.Validation;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -72,5 +72,27 @@ public class ExpertService {
 
     private boolean checkExpertStatus(Orders orders) {
         return orders.getOrderStatus().equals(OrderStatus.WAITING_EXPERT_PROPOSE) | orders.getOrderStatus().equals(OrderStatus.WAITING_EXPERT_SELECTION);
+    }
+    @Transactional
+
+    public void expertDone(Optional<Orders> orders, Optional<Offers> offers) throws OrderExistenceException {
+        Orders orders1 = checkOrder(orders);
+        if (!orders1.getOrderStatus().equals(OrderStatus.STARTING))
+            throw new OrderExistenceException("order not started!");
+
+        offers.get().setDate(new Date());
+        orders1.setOrderStatus(OrderStatus.DONE);
+    }
+    private Orders checkOrder(Optional<Orders> orders) {
+        Customer customer = orders.get().getCustomer();
+        List<Orders> ordersList = customer.getOrdersList();
+        Orders orders1 = new Orders();
+        for (Orders value : ordersList) {
+            if (value.getId().equals(orders.get().getId())) {
+                orders1 = value;
+                break;
+            }
+        }
+        return orders1;
     }
 }
